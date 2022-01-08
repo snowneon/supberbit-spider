822bfd7f57f45b454f18729c31687a728b132eac
snowneon <snowneon@gmail.com>
2022-01-08 12:28:36 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
-SuperBit.MotorRunDual(
-SuperBit.enMotors.M4,
-255,
-SuperBit.enMotors.M2,
-255
-)
+input.onButtonPressed(Button.A, function () {
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M4,
+    255,
+    SuperBit.enMotors.M2,
+    255
+    )
+})
+input.onButtonPressed(Button.B, function () {
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M4,
+    -255,
+    SuperBit.enMotors.M2,
+    -255
+    )
+})
+basic.forever(function () {
+    basic.showString("I love Betty, Nathan and Ada")
+    basic.showIcon(IconNames.Heart)
+})
